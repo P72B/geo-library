69f7dc10988b4a6538e8dff9c1378700a23a49f1
p72b <willy.riechert@googlemail.com>
2020-07-08 19:47:46 +0200

    Add basic map to directions demo.

--- app/src/main/java/de/p72b/geo/demo/showcase/MainActivity.kt
+++ app/src/main/java/de/p72b/geo/demo/showcase/MainActivity.kt
@@ -3,14 +3,18 @@ package de.p72b.geo.demo.showcase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import de.p72b.geo.demo.BR
 import de.p72b.geo.demo.R
 import de.p72b.geo.demo.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +23,12 @@ class MainActivity : AppCompatActivity() {
         viewModel = getViewModel()
         lifecycle.addObserver(viewModel)
         binding.setVariable(BR.viewmodel, viewModel)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 }
